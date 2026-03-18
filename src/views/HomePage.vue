57adbe9807eff07f36ffca1ea14307387e894d7b
Amábile Emanuelly da Luz <amabile.luz@alunos.sc.senac.br>
2026-03-18 13:26:22 -0300

    teste

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,46 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+<ion-page>
+<ion-header :translucent="true">
+<ion-toolbar>
+<ion-title>Blank</ion-title>
+</ion-toolbar>
+</ion-header>
+<ion-content :fullscreen="true">
+<ion-header collapse="condense">
+<ion-toolbar>
+<ion-title size="large">Blank</ion-title>
+</ion-toolbar>
+</ion-header>
+<div class="inicial">
+ 
+<p>Bem Vindo A tela Inicial </p>
+ 
+</div>
+</ion-content>
+<ion-button  @click="router.push('/tarefas')">ir para tarefa</ion-button>
+</ion-page>
 </template>
-
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import router from '@/router';
 </script>
-
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.inicial {
+ 
+ 
+    display: flex;
+    justify-content: center; /* horizontal */
+    align-items: center; /* vertical */
+    height: 100vh; /* altura total da viewport */
+ 
+ 
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ 
+ 
+.botao {
+ 
+   
+ 
 }
 </style>
